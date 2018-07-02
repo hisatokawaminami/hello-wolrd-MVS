32fc98b41aca149c815e2b8d3e04f0cc2e02411a
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-07-01 20:09:41 -0700

    razor

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    // [Route("/hello")]
+    // public string Hello() { return "Hello friend!"; }
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend"; }
+
+    [Produces("text/html")]
+    // [Route("/")]
+    // public ActionResult Letter()
+    // {
+    //   return View();
+    // }
+    [Route("/journal")]
+    public ActionResult Journal()
+    {
+      return View();
+    }
+    [Route("/")]
+    public ActionResult Hello()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("Jessica");
+      myLetterVariable.SetSender("John");
+
+      return View(myLetterVariable);
+    }
+  }
+}
