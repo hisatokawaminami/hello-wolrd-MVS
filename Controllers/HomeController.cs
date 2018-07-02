5ec6632c7f0ab7e31a1ac50912ec043bed63108a
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-07-01 20:36:37 -0700

    end of lesson Razor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,5 +30,19 @@ namespace FriendLetter.Controllers
 
       return View(myLetterVariable);
     }
+      [Route("/form")]
+      public ActionResult Form()
+      {
+        return View();
+      }
+      [Route("/greeting_card")]
+        public ActionResult GreetingCard()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient(Request.Query["recipient"]);
+            myLetterVariable.SetSender(Request.Query["sender"]);
+            return View("Hello", myLetterVariable);
+        }
+
   }
 }
